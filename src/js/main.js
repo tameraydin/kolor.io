3b169622a55d6b56af957897a56e3c03feb28ba4
Tamer Aydin <tamer.aydin@gmail.com>
2015-03-25 00:18:59 +0100

    enabled html5 mode

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,5 @@ var APP = require('./components/app');
 var React = require('react');
 
 React.render(
-  // <APP history={true} />,
-  <APP />,
+  <APP history={true} />,
   document.getElementById('app'));
