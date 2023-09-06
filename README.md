263cac4606c87eb8d7a567f950bb88b5843fb9d5
Tamer Aydin <tamer.aydin@gmail.com>
2023-09-06 20:53:40 +0200

    remove custom domain traces & GA tracking

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# ![](http://tamerayd.in/kolor.io/img/favicon-32x32.png) kolor.io
+# ![](https://tameraydin.github.io/kolor.io/img/favicon-32x32.png) kolor.io
 
 It is a simple color utility app that I built while experimenting with [React](http://facebook.github.io/react/) & [Flux](http://facebook.github.io/flux/).
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,6 @@
 var APP = require('./components/app');
 var React = require('react');
 
-// # tamerayd.in/kolor.io
-// # tameraydin.github.io/kolor.io
-// always use kolor.io as url-origin:
-if (window.location.origin.indexOf('tamerayd') > -1) {
-  window.location.replace('http://kolor.io/' + window.location.hash);
-}
-
 if (!window.location.hash) {
   window.location.hash = '#!/';
 }
